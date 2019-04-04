58facbd2f7ecc9fe56085d8ecf05aa56dc2c76f8
nafiantaBp1996 <nafiantabudi96@gmail.com>
2019-04-04 11:19:46 +0700

    fix data table

--- application/views/dataTransaksi.php
+++ application/views/dataTransaksi.php
@@ -8,8 +8,17 @@
             <div class="col-md-12 col-sm-12 col-xs-12">
             <div class="x_panel">
                 <div class="x_title">
-                <h1>Data Transaksi</h1>
-                <div class="clearfix"></div>
+                    <div class="row">
+                        <div class="col-md-12 col-sm-12 col-xs-12">
+                            <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+                                <h1>Data Transaksi</h1>    
+                            </div>
+                            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                                <a href="<?php echo site_url('home/form') ?>" class="btn btn-warning pull-right" style="margin-top: 15px"><span class="glyphicon glyphicon-plus-sign" style="color: white" aria-hidden="true"></span> Tambah Transaksi</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
                 </div>
                 <div class="x_content">
                 <div class="row">
@@ -19,7 +28,7 @@
                         KeyTable provides Excel like cell navigation on any table. Events (focus, blur, action etc) can be assigned to individual cells, columns, rows or all cells.
                         </p> -->
 
-                        <table id="datatable-buttons" class="table table-striped table-bordered">
+                        <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
                         <thead>
                             <tr>
                             <th>No Kredit</th>
